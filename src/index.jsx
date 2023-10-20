33c629cae6abd669e3e6bdcafb505e5a7d5f4faa
Abhay Dutt <dutt.abhay@gmail.com>
2023-10-20 06:07:32 +0530

    learn children props

--- src/index.jsx
+++ src/index.jsx
@@ -3,25 +3,50 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const author = "morgan housel";
-const title = "The Psychology Of Money";
-const img = "./images/money.jpg";
+const firstBook = {
+  author: "morgan housel",
+  title: "The Psychology Of Money",
+  img: "./images/money.jpg",
+};
+
+const secondBook = {
+  author: "hector garcia",
+  title: "Ikigai",
+  img: "./images/ikigai.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book img={img} author={author} title={title} />
-      <Book img={img} author={author} title={title} />
+      <Book
+        img={firstBook.img}
+        author={firstBook.author}
+        title={firstBook.title}
+      >
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo
+          doloribus, est qui libero eum sunt cumque aut reiciendis cum
+          veritatis, dolorum perspiciatis, dicta repudiandae facere dolore et
+          vero. Ipsa, reiciendis?
+        </p>
+        <button>Click Me</button>
+      </Book>
+      <Book
+        img={secondBook.img}
+        author={secondBook.author}
+        title={secondBook.title}
+      />
     </section>
   );
 };
 
-const Book = (props) => {
+const Book = ({ author, img, title,children }) => {
   return (
     <article className="book">
-      <img src={props.img} alt="BLAH BLAH 1" />
-      <h2>{props.title}</h2>
-      <h4>{props.author.toUpperCase()}</h4>
+      <img src={img} alt="BLAH BLAH 1" />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+      {children}
     </article>
   );
 };
